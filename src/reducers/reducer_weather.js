934e338870056199e896bf98685dc65b1a666121
AmmarCloudtech <ammar.cloudtech@gmail.com>
2017-06-17 01:36:36 +0500

    Lect 62

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,4 +1,17 @@
-export default function(state = null, action) {
-  console.log('Action received', action);
+// AVOIDING STATE MUTATIONS IN REDUCERS
+
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state = [], action) {
+  switch (action.type) {
+    case FETCH_WEATHER:
+    // we could have done state.push(action.payload.data) but that would have manipulated the existing array. Which we strictly DO NOT WANT
+    //  return state.concat([action.payload.data]); // this one and the lower one are nearly completely identical.
+      return [ action.payload.data, ...state ]; // ES6 syntax which is equivalent to above line written
+                                                // this line says make a new array and fill it with result of action.payload.data
+                                                // then take another array i.e. ...state (because of 3 dots ES6 takes it as an array)
+                                                // empty it and put its values in that newley created array (outside array).
+                                                // NOTE: action.payload.data IS INSERTED ON THE TOP OF NEWLEY CREATED ARRAY.
+  }
   return state;
 }
